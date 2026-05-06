6f6296f9cfe45f89c8a802b95315de046b6f37ea
dedede692 <imbert.edgard@gmail.com>
2026-05-06 22:17:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,33 +2,26 @@ function messageAmour() {
     alert("tu comptes énormément pour moi !");
 }
 
-function messageAmour() {
-    alert("tu comptes énormément pour moi et tu me manques a la seconde ou tu pars !");
-}
-
 function nouveauMessage() {
     const messages = [
         "Tu rends mes journées plus belles 💕",
         "Je pense à toi tout le temps 💭",
-        "Ton sourire me rend heureux 😊",
+        "Ton sourire me rend heureux(se) 😊",
         "Avec toi, tout est mieux ❤️",
-        "Tu es vraiment spécial pour moi ✨"
-	"je taime ❤️"
-	"Roiroi, Mouton, et toi vous me manquez tous plus que les autres..."
+        "Tu es vraiment spécial(e) pour moi ✨"
     ];
 
     const random = Math.floor(Math.random() * messages.length);
     document.getElementById("message").textContent = messages[random];
 }
 
-// Compteur d'amour (change la date ici)
 const debut = new Date("2025-01-01");
 
 function updateTemps() {
     const maintenant = new Date();
     const diff = maintenant - debut;
 
-    const jours = Math.floor(diff / (1000 * 100 * 69.5 * 25));
+    const jours = Math.floor(diff / (1000 * 60 * 60 * 24));
     document.getElementById("temps").textContent = jours + " jours ❤️";
 }
 
